087e555a8c02296249be5834f19602a8a6ff1c73
Dana <beware.14b@gmail.com>
2024-08-11 15:25:40 +0300

    minor tweaks

--- src/Configuration/ClimbingSpeed/ConfigClimbingSpeed.cs
+++ src/Configuration/ClimbingSpeed/ConfigClimbingSpeed.cs
@@ -1,15 +1,26 @@
+using Newtonsoft.Json;
 using Vintagestory.API.Common;
 
 namespace ConfigureEverything.Configuration.ConfigClimbingSpeed;
 
 public class ConfigClimbingSpeed : IModConfig
 {
+    [JsonProperty(Order = 1)]
     public bool Enabled { get; set; }
 
+    [JsonProperty(Order = 2)]
     public string Comment = "Ladder climbing speed";
+
+    [JsonProperty(Order = 3)]
     public float DefaultUpSpeed = 0.07f;
+
+    [JsonProperty(Order = 4)]
     public float DefaultDownSpeed = 0.035f;
+
+    [JsonProperty(Order = 5)]
     public float UpSpeed { get; set; } = 0.07f;
+
+    [JsonProperty(Order = 6)]
     public float DownSpeed { get; set; } = 0.035f;
 
     public ConfigClimbingSpeed(ICoreAPI api, ConfigClimbingSpeed previousConfig = null)
